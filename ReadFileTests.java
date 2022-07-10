859d3b1b8695018d0ad0f4393344b7f1e9480883
Pavel Shnurov <pavel.shnurov@gmail.com>
2022-07-10 21:17:49 +0300

    change class WithProblemsTests

---
+++ ReadFileTests.java
@@ -0,0 +1,11 @@
+lines (7 sloc)  160 Bytes
+
+import org.junit.jupiter.api.Test;
+
+public class ReadFileTests {
+
+    @Test
+    public void secondLineShouldHabAlloha(){
+        //write here second task
+    }
+}
