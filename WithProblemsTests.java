de5e677ba7eb4bfe5d5e4ce890378ca4ba9ead1f
Pavel Shnurov <pavel.shnurov@gmail.com>
2022-07-10 20:59:27 +0300

    change class WithProblemsTests

--- WithProblemsTests.java
+++ WithProblemsTests.java
@@ -1,2 +1,54 @@
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 public class WithProblemsTests {
+
+    private final String CONST = "const";
+    private final static String bd = "test";
+
+    @Test
+    public void equalsOneToOne(){
+        assertEquals("1", "1");
+    }
+
+
+    @Test
+    public void assignValueToConstVar(){
+        CONST = "const";
+        assertEquals("const", CONST);
+    }
+
+    @Test
+    public static void equalsOneToOne(){
+        assertEquals(1, 1);
+    }
+
+
+
+    @Test
+    public void stringsMustBeEquals(){
+        String res = "a";
+
+        if (bd == new String("test")) {
+            res = "test";
+        }
+
+        assertEquals("test", res);
+    }
+
+    @Test
+    public void successfullyRemovingFirstElementFromList(){
+        List<String> sourceData = List.of("2", "fin", "bin");
+        for (String element: sourceData){
+            sourceData.remove(element);
+        }
+        assertFalse(sourceData.contains("1"));
+    }
+
+
 }
